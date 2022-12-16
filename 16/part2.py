4f6d80721a29b8b2ba8cb342c2d63ed176a3bb2d
Duddino Gatto <duddinogatto@pm.me>
2022-12-16 22:45:37 +0100

    Update

--- 16/part2.py
+++ 16/part2.py
@@ -7,66 +7,71 @@ for line in open(0).read().splitlines():
     flow_rate = int(line.split("=")[1].split(";")[0])
     tunnels = re.split("valves? ", line)[1].split(", ")
     valves[valve] = (flow_rate, tunnels)
+non_empty = [v for v in valves if valves[v][0] != 0]
 
-
+cache = {}
 def path(a, b):
+    if (a, b) in cache:
+        return cache[a, b]
     queue = deque()
-    queue.append(a)
-    traceback = dict()
+    queue.append((a, 0))
+    traceback = set()
     while len(queue) >= 0:
-        current = queue.popleft()
+        current, length = queue.popleft()
         if current == b:
-            break
+            cache[a, b] = length+1
+            return length + 1
         _, neighbors = valves[current]
         for neighbor in neighbors:
             if neighbor not in traceback:
-                queue.append(neighbor)
-                traceback[neighbor] = current
-    if b in traceback:
-        path = [b]
-        current = b
-        while current != a:
-            current = traceback[current]
-            path.append(current)
-        return [i for i in reversed(path)]
-    else:
-        return None
+                queue.append((neighbor, length + 1))
+                traceback.add(neighbor)
 
 def bfs(valves, starting, minutes):
-    queue = deque()
+    queue = []
     queue.append((starting, starting, set(), (minutes, minutes), 0))
     max_pressure = 0
     thing = [i for i in valves]
-    seen = set()
     while len(queue) > 0:
-        print(len(queue))
-        me, elephant, opened, minutes, pressure = queue.popleft()
+        me, elephant, opened, minutes, pressure = queue.pop()
+        if minutes[0] <= 0 and minutes[1] <= 0:
+            continue
         if pressure > max_pressure:
+            print("New pressure!" + str(max_pressure))
             max_pressure = pressure
-        for i, elephant_valve in enumerate(thing):
-            flow_rate_elephant, _ = valves[elephant_valve]
-            if elephant_valve in opened:
-                continue
-            if flow_rate_elephant == 0:
-                continue
-            for j in range(0, len(valves)):
-                me_valve = thing[j]
-                if elephant_valve == me_valve:
+        if (minutes[0] > 0 and minutes[1] > 0) and (elephant != None):
+            for elephant_valve in non_empty:
+                flow_rate_elephant, _ = valves[elephant_valve]
+                if elephant_valve in opened:
                     continue
-                flow_rate_me, _ = valves[me_valve]
-                if flow_rate_me == 0:
+                for me_valve in non_empty:
+                    flow_rate_me, _ = valves[me_valve]
+                    if me_valve == elephant_valve:
+                        continue
+                    if me_valve in opened:
+                        continue
+                    # Add my and elephant valve to queue
+                    e_path = path(elephant, elephant_valve)
+                    me_path = path(me, me_valve)
+                    c = opened.copy()
+                    c.add(elephant_valve)
+                    c.add(me_valve)
+                    pressuree = flow_rate_me * (minutes[0] - (me_path)), flow_rate_elephant * (minutes[1] - (e_path))
+                    pressure_adj = pressuree[0] if pressuree[0] > 0 else 0, pressuree[1] if pressuree[1] > 0 else 0
+                    queue.append((me_valve, elephant_valve, c, (minutes[0] - (me_path), minutes[1] - (e_path)), pressure + sum(pressure_adj)))
+        else:
+            for valve in non_empty:
+                flow_rate, _ = valves[valve]
+                if valve in opened:
                     continue
-                if me_valve in opened:
-                    continue
-                # Add my and elephant valve to queue
-                e_path = path(elephant, elephant_valve)
-                me_path = path(me, me_valve)
+                pathi = path(me, valve)
                 c = opened.copy()
-                c.add(elephant_valve)
-                c.add(me_valve)
-                pressuree = flow_rate_me * (minutes[0] - len(me_path)), flow_rate_elephant * (minutes[1] - len(e_path))
-                pressure_adj = pressuree[0] if pressuree[0] > 0 else 0, pressuree[1] if pressuree[1] > 0 else 0
-                queue.append((me_valve, elephant_valve, c, (minutes[0] - len(me_path), minutes[1] - len(e_path)), pressure + sum(pressure_adj)))
+                c.add(valve)
+                non_negative = minutes[0] if minutes[0] > 0 else minutes[1]
+                pressuree = flow_rate * (non_negative - pathi)
+                pressure_adj = pressuree if pressuree > 0 else 0
+                queue.append((valve, None, c, (minutes[0] - (me_path), -10), pressure + pressure_adj))
+                    
     return max_pressure
     
             
